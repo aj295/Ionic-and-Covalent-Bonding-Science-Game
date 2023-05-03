0df41e6011beb227647e358f8a749fd133f158d4
aj295 <120733593+aj295@users.noreply.github.com>
2023-05-02 20:48:54 -0400

    deleted test to make another test

--- character/character.js
+++ character/character.js
@@ -275,10 +275,6 @@ export default class Character {
         else this.airTime = 0
     }
 
-    test() {
-        console.log("this is a test")
-    }
-
     /**
      * @description offset the character's x and y values with the distance parameter over a certain amount of time
      * @param {number} time 
