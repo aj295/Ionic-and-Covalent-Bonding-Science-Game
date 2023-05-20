fa287d95454ede93df9e247441dac888a305be7f
aj295 <120733593+aj295@users.noreply.github.com>
2023-05-20 00:15:54 -0400

    changed stuff

--- line.js
+++ line.js
@@ -33,12 +33,9 @@ export default class Line {
     }
 
     update(x1, y1, x2, y2) {
-
         this.x1 += x1
         this.y1 += y1
         this.x2 += x2
         this.y2 += y2
-        
-        this.draw()
     }
 }
